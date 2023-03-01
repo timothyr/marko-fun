4574a0f981e704843b6273a26f698ce10f2bb97c
Tim Robertson <timr@sfu.ca>
2023-02-28 22:45:05 -0800

    Minify css

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,12 @@
 import tailwind from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
+import cssnano from 'cssnano'
 import tailwindConfig from './tailwind.config.js'
 
 export default {
-  plugins: [tailwind(tailwindConfig), autoprefixer],
+  plugins: [
+    tailwind(tailwindConfig), 
+    autoprefixer,
+    ...(process.env.NODE_ENV === 'production' ? [cssnano] : [])
+  ]
 }

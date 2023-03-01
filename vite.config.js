af094e4bf3fe6a4dff3326bc6195b2105de7a0f0
Tim Robertson <timr@sfu.ca>
2023-02-28 21:25:03 -0800

    Add tailwindcss

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from "vite";
 import marko from "@marko/vite";
+import postcss from './postcss.config.js';
 
 export default defineConfig({
   plugins: [marko()],
   build: {
+    minify: true,
     sourcemap: true, // Generate sourcemaps for all builds.
     emptyOutDir: false, // Avoid server & client deleting files from each other.
   },
+  css: {
+    postcss,
+  }
 });
